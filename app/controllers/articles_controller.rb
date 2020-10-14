1bc85f71c588aa4689f50b8f244d3a99fcdbcf2c
Victor Garcia Mestre <vgmestre@gmail.com>
2020-10-14 23:28:29 +0200

    articles controller: new action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,9 @@ class ArticlesController < ApplicationController
         @articles = Article.all 
     end
 
+    def new
+    end
+    
     def show
         @article = Article.find(params[:id])
     end
